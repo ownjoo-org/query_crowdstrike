d4e84aa511606405f6f255aa141e802c2c69db1b
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2023-11-04 09:51:17 -0500

    add scripts lookup; update readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # query_crowdstrike
 test query for crowdstrike API host
+
+# usage
+```
+$ python main.py --help
+usage: main.py [-h] --client-id CLIENT_ID --client-secret CLIENT_SECRET [--hostname HOSTNAME] [--filter FILTER] [--domain DOMAIN] [--proxies PROXIES]
+               [--list-vulns] [--list-scripts]
+
+options:
+  -h, --help                     show this help message and exit
+  --client-id CLIENT_ID          The client_id of the API key
+  --client-secret CLIENT_SECRET  The API key
+  --hostname HOSTNAME            The hostname to query for
+  --filter FILTER                The query filter to use to filter the agent ID query results
+  --domain DOMAIN                The FQDN for your Crowdstrike account's API (not full URL)
+  --proxies PROXIES              JSON structure specifying 'http' and 'https' proxy URLs
+  --list-vulns                   Get vulnerabilities for the specified host
+  --list-scripts                 List RTR custom scripts details
+```
+
+
+# example: look up host with vulnerability details
+`python3 main.py --domain api.crowdstrike.com --client-id blah --client-secret blah --hostname abc123 --list-vulns`
+
+# example: list all custom scripts
+`python3 main.py --domain api.crowdstrike.com --client-id blah --client-secret blah --list-scripts`
+
--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def main(
         client_secret: str,
         query_filter: str,
         proxies: Optional[dict] = None,
-        get_vulns: bool = False,
+        list_vulns: bool = False,
+        list_scripts: bool = False,
 ):
     session = Session()
 
@@ -22,139 +23,190 @@ def main(
     aid: Optional[str] = None
     device: Optional[dict] = None
 
+    print(f'domain: {domain}')
+    print(f'client_id: {client_id}')
+    print(f'client_secret: {client_secret}')
+    print(f'query_filter: {query_filter}')
+    print(f'proxies: {proxies}')
+    print(f'list_vulns: {list_vulns}')
+    print(f'list_scripts: {list_scripts}')
     # authenticate
-    try:
-        body_params = {
-            'client_id': client_id,
-            'client_secret': client_secret,
-        }
-        resp_auth: Response = session.post(
-            url=f'https://{domain}/oauth2/token',
-            data=body_params,
-            proxies=proxies,
-        )
-        resp_auth.raise_for_status()
-        data_auth: dict = resp_auth.json()
-        token = data_auth.get('access_token')
-        headers['Authorization'] = f'Bearer {token}'
-    except Exception as exc_auth:
-        print(f'{exc_auth}')
+    # try:
+    #     body_params = {
+    #         'client_id': client_id,
+    #         'client_secret': client_secret,
+    #     }
+    #     resp_auth: Response = session.post(
+    #         url=f'https://{domain}/oauth2/token',
+    #         data=body_params,
+    #         proxies=proxies,
+    #     )
+    #     resp_auth.raise_for_status()
+    #     data_auth: dict = resp_auth.json()
+    #     token = data_auth.get('access_token')
+    #     headers['Authorization'] = f'Bearer {token}'
+    # except Exception as exc_auth:
+    #     print(f'{exc_auth}')
 
-    # query for agent ID
-    try:
-        resp_query: Response = session.get(
-            url=f'https://{domain}/devices/queries/devices/v1',
-            params={'filter': query_filter},
-            headers=headers,
-            proxies=proxies,
-        )
-        resp_query.raise_for_status()
-        data_query: dict = resp_query.json()
-        resources: list = data_query.get('resources')
-        for resource in resources:
-            aid = resource
-        print(f'id: {aid}')
-    except Exception as exc_query:
-        print(f'{exc_query}')
-
-    # query for device data
-    try:
-        resp_entity: Response = session.get(
-            url=f'https://{domain}/devices/entities/devices/v1',
-            params={'ids': aid},
-            headers=headers,
-            proxies=proxies,
-        )
-        resp_entity.raise_for_status()
-        data_entity: dict = resp_entity.json()
-        resources: list = data_entity.get('resources')
-        for resource in resources:
-            device = resource
-        print(f'device: {device}')
-    except Exception as exc_entity:
-        print(f'{exc_entity}')
-
-    # query for vulns data
-    if get_vulns:
-        vulnerabilities: list = []
+    if query_filter:
+        # query for agent ID
         try:
-            query_filters: List[str] = [f'aid:\'{aid}\'']
-            url_params: dict = {
-                'filter': '+'.join(query_filters),
-                # 'limit': 1,
-                # 'facet': ['cve', 'remediation'],
-            }
-            resp_vuln: Response = session.get(
-                url=f'https://{domain}/spotlight/combined/vulnerabilities/v1',
-                params=url_params,
+            resp_query: Response = session.get(
+                url=f'https://{domain}/devices/queries/devices/v1',
+                params={'filter': query_filter},
                 headers=headers,
                 proxies=proxies,
             )
-            resp_vuln.raise_for_status()
-            data_vuln: dict = resp_vuln.json()
-            resources: list = data_vuln.get('resources')
+            resp_query.raise_for_status()
+            data_query: dict = resp_query.json()
+            resources: list = data_query.get('resources')
             for resource in resources:
-                vulnerabilities.append(resource)
+                aid = resource
+            print(f'id: {aid}')
+        except Exception as exc_query:
+            print(f'{exc_query}')
+
+        # query for device data
+        try:
+            resp_entity: Response = session.get(
+                url=f'https://{domain}/devices/entities/devices/v1',
+                params={'ids': aid},
+                headers=headers,
+                proxies=proxies,
+            )
+            resp_entity.raise_for_status()
+            data_entity: dict = resp_entity.json()
+            resources: list = data_entity.get('resources')
+            for resource in resources:
+                device = resource
             print(f'device: {device}')
-            device['vulnerabilities'] = vulnerabilities
         except Exception as exc_entity:
             print(f'{exc_entity}')
-    return device
+
+        # query for vulns data
+        if list_vulns:
+            vulnerabilities: list = []
+            try:
+                query_filters: List[str] = [f'aid:\'{aid}\'']
+                url_params: dict = {
+                    'filter': '+'.join(query_filters),
+                    # 'limit': 1,
+                    # 'facet': ['cve', 'remediation'],
+                }
+                resp_vuln: Response = session.get(
+                    url=f'https://{domain}/spotlight/combined/vulnerabilities/v1',
+                    params=url_params,
+                    headers=headers,
+                    proxies=proxies,
+                )
+                resp_vuln.raise_for_status()
+                data_vuln: dict = resp_vuln.json()
+                resources: list = data_vuln.get('resources')
+                for resource in resources:
+                    vulnerabilities.append(resource)
+                print(f'device: {device}')
+                device['vulnerabilities'] = vulnerabilities
+            except Exception as exc_entity:
+                print(f'{exc_entity}')
+        return device
+
+    elif list_scripts:
+        # query for script IDs
+        script_ids: list = []
+        try:
+            resp_query: Response = session.get(
+                url=f'https://{domain}/real-time-response/queries/scripts/v1',
+                params={'filter': query_filter},
+                headers=headers,
+                proxies=proxies,
+            )
+            resp_query.raise_for_status()
+            data_query: dict = resp_query.json()
+            script_ids = data_query.get('resources') or []
+        except Exception as exc_query:
+            print(f'{exc_query}')
+
+        # query for scripts details
+        try:
+            resp_entity: Response = session.get(
+                url=f'https://{domain}/real-time-response/entities/scripts/v2',
+                params={'ids': script_ids},
+                headers=headers,
+                proxies=proxies,
+            )
+            resp_entity.raise_for_status()
+            data_entity: dict = resp_entity.json()
+            resources: list = data_entity.get('resources')
+            print(json.dumps(resources, indent=4))
+        except Exception as exc_entity:
+            print(f'{exc_entity}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--client_id',
+        '--client-id',
+        default=None,
         type=str,
-        help='The client_id of the API key',
         required=True,
-        default=None
+        help='The client_id of the API key',
+        dest='client_id',
     )
     parser.add_argument(
-        '--client_secret',
+        '--client-secret',
+        default=None,
         type=str,
-        help='The API key',
         required=True,
-        default=None
+        help='The API key',
+        dest='client_secret',
     )
     parser.add_argument(
         '--hostname',
+        default=None,
         type=str,
-        help='The hostname to query for',
         required=False,
-        default=None
+        help='The hostname to query for',
     )
     parser.add_argument(
         '--filter',
+        default=None,
         type=str,
-        help='The query filter to use to filter the agent ID query results',
         required=False,
-        default=None
+        help='The query filter to use to filter the agent ID query results',
     )
     parser.add_argument(
         '--domain',
+        default='api.crowdstrike.com',
         type=str,
-        help="The FQDN for your Crowdstrike account's API",
         required=False,
-        default='api.crowdstrike.com'
+        help="The FQDN for your Crowdstrike account's API (not full URL)",
     )
     parser.add_argument(
         '--proxies',
         type=str,
-        help="JSON structure specifying 'http' and 'https' proxy URLs",
         required=False,
+        help="JSON structure specifying 'http' and 'https' proxy URLs",
     )
     parser.add_argument(
-        '--vulns',
-        type=bool,
-        help="Get vulnerabilities for the specified host",
+        '--list-vulns',
+        action='store_true',
         default=False,
         required=False,
+        help="Get vulnerabilities for the specified host",
+        dest='list_vulns',
     )
+    parser.add_argument(
+        '--list-scripts',
+        action='store_true',
+        default=False,
+        required=False,
+        help="List RTR custom scripts details",
+        dest='list_scripts',
+    )
+
     args = parser.parse_args()
 
-    query_filter: str = ''
+    query_filter: Optional[str] = None
     if args.hostname and not args.filter:
         query_filter = f"hostname:'{args.hostname}'"
     elif args.filter:
@@ -173,7 +225,8 @@ if __name__ == '__main__':
         client_secret=args.client_secret,
         query_filter=query_filter,
         proxies=proxies,
-        get_vulns=args.vulns,
+        list_vulns=args.list_vulns,
+        list_scripts=args.list_scripts,
     )
 
     if device:
